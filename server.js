9b80adf0dd4d17717c3b24218a4ff84ef59984f7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-22 06:47:02 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.send("Our express api server is now sending this over to the browser");
+});
+
+
+const server = app.listen(PORT, () =>
+  console.log(`🦄 🦄 🦄 Server is running and listening on Port ${server.address().port}! 🦄 🦄 🦄`)
+);
