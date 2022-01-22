a664354ecac204fea85b03ad5a9dc5fcb4d55a5c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-22 07:23:51 -0800

    add Get a Random Name from Faker API in server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+// **** Express ********
 const express = require("express");
 const app = express();
 const PORT = 8000;
+// **** Faker *******
+const faker = require('@faker-js/faker');
 
 ///////////////////////////////////////////////
 //  SERVER JAVASCRIPT
 ///////////////////////////////////////////////
 
+const randomName = faker.name.findName();
+
 //  //// ROUTES ///////////////////////////////
 //    req is short for request, res is short for response
 app.get("/api", (req, res) => {
-  res.send("Our express api server is now sending this over to the browser");
+  // res.send("Our express api server is now sending this over to the browser");
+  res.send(`Faker name: ${randomName}`)
 });
 
 
